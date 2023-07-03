65ca83aa10ee9436f0e6a2ecbf8fd88bd9cefa9e
Guilherme Lazani Lopes <guilherme-lazarini@hotmail.com>
2023-07-02 21:33:53 -0300

    Add Categoria Controller

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APICatalogo.Context;
+using APICatalogo.Models;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ApiCatalogoDbContext _context;
+
+        public CategoriasController(ApiCatalogoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+          if (_context.Categorias == null)
+          {
+              return NotFound();
+          }
+            return await _context.Categorias.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> GetCategoria(Guid id)
+        {
+          if (_context.Categorias == null)
+          {
+              return NotFound();
+          }
+            var categoria = await _context.Categorias.FindAsync(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return categoria;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria(Guid id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+          if (_context.Categorias == null)
+          {
+              return Problem("Entity set 'ApiCatalogoDbContext.Categorias'  is null.");
+          }
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoria(Guid id)
+        {
+            if (_context.Categorias == null)
+            {
+                return NotFound();
+            }
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoriaExists(Guid id)
+        {
+            return (_context.Categorias?.Any(e => e.CategoriaId == id)).GetValueOrDefault();
+        }
+    }
+}
